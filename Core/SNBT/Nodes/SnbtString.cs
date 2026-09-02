fa6bb30aefd12b6f631fe47b86bc9bd787a3a34e
SkyReed <itzskyreed@gmail.com>
2026-09-02 20:00:57 +0500

    feat: Improved SnbtString escaping

--- Core/SNBT/Nodes/SnbtString.cs
+++ Core/SNBT/Nodes/SnbtString.cs
@@ -2,11 +2,23 @@
 
 namespace Core.SNBT.Nodes;
 
+/// <summary>
+/// Represents a string value node in an SNBT structure.
+/// </summary>
 public record SnbtString(string Value) : ISnbtNode
 {
-    public string ToSnbtString(bool pretty = true, string indent = "")
+    public string ToSnbtString(bool pretty = false, string indent = "")
     {
-        var escaped = Value.Replace("\"", "\\\"");
+
+        var escaped = Value
+            .Replace("\\", @"\\")
+            .Replace("\"", "\\\"")
+            .Replace("\b", "\\b")
+            .Replace("\f", "\\f")
+            .Replace("\n", "\\n")
+            .Replace("\r", "\\r")
+            .Replace("\t", "\\t");
+
         return $"\"{escaped}\"";
     }
 }
