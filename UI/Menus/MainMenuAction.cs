eb314bd71a6c4d64410b5a1e41b84d6b6ddd03f5
SkyReed <itzskyreed@gmail.com>
2026-09-02 20:05:43 +0500

    feat(UI): added main menu

---
+++ UI/Menus/MainMenuAction.cs
@@ -0,0 +1,35 @@
+﻿using UI.Actions.Common;
+using UI.Interfaces;
+using UI.Styling;
+
+namespace UI.Menus;
+
+/// <summary>
+/// The root menu of the application.
+/// </summary>
+public class MainMenuAction(IEnumerable<IMainMenuAction> menuActions) : ITuiAction
+{
+    public string Title => "Main Menu";
+
+    public async Task ExecuteAsync()
+    {
+        // Add a "Quit" option dynamically
+        var choices = menuActions.ToList<ITuiAction>();
+        choices.Add(new BackAction("[grey]Exit[/]"));
+
+        while (true)
+        {
+            TuiTheme.RenderHeader("BACAP Manager");
+
+            var selected = TuiTheme.PromptSelection(
+                "Choose actions",
+                choices,
+                action => action.Title);
+
+            if (selected is BackAction)
+                break;
+
+            await selected.ExecuteAsync();
+        }
+    }
+}
