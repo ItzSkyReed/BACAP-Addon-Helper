a639a0581fcf2197dddb9738868ed6afdd1d1be7
SkyReed <itzskyreed@gmail.com>
2026-09-02 19:52:19 +0500

    feat: Updated readonly datapack interface

--- BacapGenerator/Models/Interfaces/IReadOnlyDatapack.cs
+++ BacapGenerator/Models/Interfaces/IReadOnlyDatapack.cs
@@ -1,13 +1,21 @@
-﻿using BacapGenerator.Models.Datapacks.Settings;
+﻿using BacapGenerator.Models.Advancements;
+using BacapGenerator.Models.Datapacks.Settings;
 using Core.Registries;
 
 namespace BacapGenerator.Models.Interfaces;
 
 public interface IReadOnlyDatapack
 {
+    /// <summary>
+    /// Gets the unique identifier of the datapack as defined in the configuration.
+    /// </summary>
+    public string Id { get; }
+
+    public IReadOnlyList<ManagedAdvancement> Advancements { get; }
+
     DatapackSettings Settings { get; }
 
-    public DirectoryInfo DatapackDataPath { get;  }
+    public DirectoryInfo DatapackDataPath { get; }
 
     public MinecraftData MinecraftData { get; }
 }
