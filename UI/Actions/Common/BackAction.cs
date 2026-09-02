54127050349627ae30e6bb0b708d3ee96ceabf83
SkyReed <itzskyreed@gmail.com>
2026-09-02 20:06:55 +0500

    feat: Added back action

---
+++ UI/Actions/Common/BackAction.cs
@@ -0,0 +1,16 @@
+﻿using UI.Interfaces;
+using UI.Styling;
+
+namespace UI.Actions.Common;
+
+/// <summary>
+/// A reusable dummy action to represent navigation options like "Back" or "Exit" in menus.
+/// It is meant to be intercepted by the menu loop to break out of it.
+/// </summary>
+/// <param name="title">The display title of the navigation action. Defaults to "[grey]Back[/]".</param>
+public class BackAction(string title = TuiTheme.BackOptionString) : ITuiAction
+{
+    public string Title { get; } = title;
+
+    public Task ExecuteAsync() => Task.CompletedTask;
+}
