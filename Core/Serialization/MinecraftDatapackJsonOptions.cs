d422c2aa86dc1351e1fe5307e49221fe7dbff82e
SkyReed <itzskyreed@gmail.com>
2026-09-17 21:56:31 +0500

    chore: Added BaseTranslation JsonSerializerOptions

--- Core/Serialization/MinecraftDatapackJsonOptions.cs
+++ Core/Serialization/MinecraftDatapackJsonOptions.cs
@@ -23,4 +23,16 @@ public static class MinecraftDatapackJsonOptions
         // Disables aggressive HTML-escaping, keeping characters like ' and & as they are
         Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
     };
+
+    /// <summary>
+    /// The default JSON options enforcing snake_case naming and ignoring null values.
+    /// </summary>
+    public static readonly JsonSerializerOptions BaseTranslation = new()
+    {
+        WriteIndented = true,
+        IndentSize = 2,
+
+        // Disables aggressive HTML-escaping, keeping characters like ' and & as they are
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
 }
